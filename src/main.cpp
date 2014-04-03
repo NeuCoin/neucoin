e13b53c78a4e333dd5a09b677d64076b493fc456
JSmith-BitFlipper <db.htam@gmail.com>
2014-04-03 17:19:20 -0400

    Better memory management in ProcessBlock()
    In the shunting area on line 2031, where blocks that do not have their previous block yet are handled, there is a special case for proof-of-stake blocks. That case can fail and a return false is called. Above that there is a creation of a CBlock by new. If the return false is reached, that CBlock* that was allocated space is not handled. All I added was a delete pblock2; to handle memory better.

--- src/main.cpp
+++ src/main.cpp
@@ -2089,7 +2089,10 @@ bool ProcessBlock(CNode* pfrom, CBlock* pblock)
             // Duplicate stake allowed only when there is orphan child block
             if (setStakeSeenOrphan.count(pblock2->GetProofOfStake()) && !mapOrphanBlocksByPrev.count(hash) && !Checkpoints::WantedByPendingSyncCheckpoint(hash))
             {
-                return error("ProcessBlock() : duplicate proof-of-stake (%s, %d) for orphan block %s", pblock2->GetProofOfStake().first.ToString().c_str(), pblock2->GetProofOfStake().second, hash.ToString().c_str());
+                error("ProcessBlock() : duplicate proof-of-stake (%s, %d) for orphan block %s", pblock2->GetProofOfStake().first.ToString().c_str(), pblock2->GetProofOfStake().second, hash.ToString().c_str());
+                //pblock2 will not be needed, free it
+                delete pblock2;
+                return false;
             }
             else
             {
