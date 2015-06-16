18be85ff16af07df65c2157841326b36fef6948c
Maël Nison <nison.mael@gmail.com>
2015-06-16 15:38:32 +0200

    Alignates constants

--- src/constants.h
+++ src/constants.h
@@ -23,70 +23,70 @@
 
 #define COIN_BUGTRACKER  "http://github.com/neucoin/neucoind/issues"
 
-extern uint16_t      COIN_PORT;
-extern uint16_t      RPC_PORT;
+extern uint16_t                          COIN_PORT;
+extern uint16_t                          RPC_PORT;
 
-extern uint8_t       PROTOCOL_MAGIC_BYTES[4];
+extern uint8_t                           PROTOCOL_MAGIC_BYTES[4];
 
-extern char const * (*AUTO_DNS_SEEDS)[2];
-extern uint32_t     (*AUTO_IP_SEEDS);
+extern char const *                     (*AUTO_DNS_SEEDS)[2];
+extern uint32_t                         (*AUTO_IP_SEEDS);
 
-extern uint8_t       PUBKEY_ADDRESS_PREFIX;
-extern uint8_t       PRVKEY_ADDRESS_PREFIX;
-extern uint8_t       SCRIPT_ADDRESS_PREFIX;
+extern uint8_t                           PUBKEY_ADDRESS_PREFIX;
+extern uint8_t                           PRVKEY_ADDRESS_PREFIX;
+extern uint8_t                           SCRIPT_ADDRESS_PREFIX;
 
-extern hash_t        GENESIS_MERKLE_HASH;
-extern hash_t        GENESIS_HASH;
-extern char const *  GENESIS_IDENT;
-extern timestamp_t   GENESIS_TX_TIME;
-extern timestamp_t   GENESIS_BLOCK_TIME;
-extern uint32_t      GENESIS_BLOCK_NONCE;
-extern uint32_t      GENESIS_BLOCK_VERSION;
+extern hash_t                            GENESIS_MERKLE_HASH;
+extern hash_t                            GENESIS_HASH;
+extern char const *                      GENESIS_IDENT;
+extern timestamp_t                       GENESIS_TX_TIME;
+extern timestamp_t                       GENESIS_BLOCK_TIME;
+extern uint32_t                          GENESIS_BLOCK_NONCE;
+extern uint32_t                          GENESIS_BLOCK_VERSION;
 
 extern std::map<blockheight_t, hash_t>   BLOCK_CHECKPOINTS;
 extern std::map<blockheight_t, uint32_t> STAKE_MODIFIER_CHECKPOINTS;
 
-extern blockheight_t COINBASE_MATURITY;
+extern blockheight_t                     COINBASE_MATURITY;
 
-extern money_t       MAX_MONEY_STACK;
-extern money_t       COIN_PREMINE;
+extern money_t                           MAX_MONEY_STACK;
+extern money_t                           COIN_PREMINE;
 
-extern money_t       MIN_TX_FEES;
-extern money_t       MIN_RELAY_TX_FEES;
+extern money_t                           MIN_TX_FEES;
+extern money_t                           MIN_RELAY_TX_FEES;
 
-extern timestamp_t   MAX_CLOCK_DRIFT;
+extern timestamp_t                       MAX_CLOCK_DRIFT;
 
-extern uint32_t      MAX_BLOCK_SIZE;
+extern uint32_t                          MAX_BLOCK_SIZE;
 
-extern blockheight_t POW_MAX_BLOCK;
+extern blockheight_t                     POW_MAX_BLOCK;
 
-extern target_t      POW_INITIAL_TARGET;
-extern target_t      POS_INITIAL_TARGET;
+extern target_t                          POW_INITIAL_TARGET;
+extern target_t                          POS_INITIAL_TARGET;
 
-extern target_t      POW_MAX_TARGET;
-extern target_t      POS_MAX_TARGET;
+extern target_t                          POW_MAX_TARGET;
+extern target_t                          POS_MAX_TARGET;
 
-extern timestamp_t   POW_TARGET_SPACING;
-extern timestamp_t   POS_TARGET_SPACING;
+extern timestamp_t                       POW_TARGET_SPACING;
+extern timestamp_t                       POS_TARGET_SPACING;
 
-extern money_t       POW_BLOCK_REWARD;
+extern money_t                           POW_BLOCK_REWARD;
 
-extern timestamp_t   STAKE_MIN_AGE;
-extern timestamp_t   STAKE_MAX_AGE;
+extern timestamp_t                       STAKE_MIN_AGE;
+extern timestamp_t                       STAKE_MAX_AGE;
 
-extern money_t       STAKE_COIN_STEP;
-extern timestamp_t   STAKE_AGE_STEP;
+extern money_t                           STAKE_COIN_STEP;
+extern timestamp_t                       STAKE_AGE_STEP;
 
-extern timestamp_t   TARGET_TIMESPAN;
+extern timestamp_t                       TARGET_TIMESPAN;
 
-extern std::string   CHECKPOINT_PUBLIC_KEY;
-extern std::string   CHECKPOINT_PRIVATE_KEY;
+extern std::string                       CHECKPOINT_PUBLIC_KEY;
+extern std::string                       CHECKPOINT_PRIVATE_KEY;
 
-extern money_t       MIN_TXOUT_AMOUNT;
+extern money_t                           MIN_TXOUT_AMOUNT;
 
-extern uint32_t      MAX_BLOCK_SIZE_GEN;
-extern uint32_t      MAX_BLOCK_SIGOPS;
-extern uint32_t      MAX_BLOCK_ORPHAN_TX;
+extern uint32_t                          MAX_BLOCK_SIZE_GEN;
+extern uint32_t                          MAX_BLOCK_SIGOPS;
+extern uint32_t                          MAX_BLOCK_ORPHAN_TX;
 
 // -- And the following is just miscellaneous macro tools - please don't add any more except if it's for a really good reason
 
--- src/testnet.cpp
+++ src/testnet.cpp
@@ -13,30 +13,30 @@ static uint32_t     TESTNET_AUTO_IP_SEEDS[]                                    =
 void ApplyTestnetParameters(void)
 {
 
-    COIN_PORT                        = 8742;
-    RPC_PORT                         = 8743;
+    COIN_PORT                  = 8742;
+    RPC_PORT                   = 8743;
 
     std::memcpy(PROTOCOL_MAGIC_BYTES, &TESTNET_PROTOCOL_MAGIC_BYTES, sizeof(PROTOCOL_MAGIC_BYTES));
 
-    AUTO_DNS_SEEDS                   = TESTNET_AUTO_DNS_SEEDS;
-    AUTO_IP_SEEDS                    = TESTNET_AUTO_IP_SEEDS;
+    AUTO_DNS_SEEDS             = TESTNET_AUTO_DNS_SEEDS;
+    AUTO_IP_SEEDS              = TESTNET_AUTO_IP_SEEDS;
 
-    PUBKEY_ADDRESS_PREFIX            =  65; // 'T'
-    PRVKEY_ADDRESS_PREFIX            =  68; // 'U'
-    SCRIPT_ADDRESS_PREFIX            = 127; // 't'
+    PUBKEY_ADDRESS_PREFIX      =  65; // 'T'
+    PRVKEY_ADDRESS_PREFIX      =  68; // 'U'
+    SCRIPT_ADDRESS_PREFIX      = 127; // 't'
 
-    GENESIS_MERKLE_HASH              = hash_t("0xa7fc40033828e0b0aa5f829ad35233efe8a15e5820d31e53d8e865a1c29cf024");
-    GENESIS_HASH                     = hash_t("0x000002f38bfabee423e760cbbbd2147984be70a531928b89e64d59531b136944");
-    GENESIS_IDENT                    = "06-15-15 :: LFG 887 :: We can still take them out for an overpriced dinner";
-    GENESIS_TX_TIME                  = 1345083810;
-    GENESIS_BLOCK_TIME               = 1345084287;
-    GENESIS_BLOCK_NONCE              = 2179980296;
-    GENESIS_BLOCK_VERSION            = 1;
+    GENESIS_MERKLE_HASH        = hash_t("0xa7fc40033828e0b0aa5f829ad35233efe8a15e5820d31e53d8e865a1c29cf024");
+    GENESIS_HASH               = hash_t("0x000002f38bfabee423e760cbbbd2147984be70a531928b89e64d59531b136944");
+    GENESIS_IDENT              = "06-15-15 :: LFG 887 :: We can still take them out for an overpriced dinner";
+    GENESIS_TX_TIME            = 1345083810;
+    GENESIS_BLOCK_TIME         = 1345084287;
+    GENESIS_BLOCK_NONCE        = 2179980296;
+    GENESIS_BLOCK_VERSION      = 1;
 
     BLOCK_CHECKPOINTS          = boost::assign::map_list_of( 0, GENESIS_HASH );
     STAKE_MODIFIER_CHECKPOINTS = boost::assign::map_list_of( 0, 0xfd11f4e7 );
 
-    POW_MAX_BLOCK                    = std::numeric_limits< blockheight_t >::max( );
+    POW_MAX_BLOCK              = std::numeric_limits< blockheight_t >::max( );
 
-    COIN_PREMINE                     = 0 * COIN;
+    COIN_PREMINE               = 0 * COIN;
 }
